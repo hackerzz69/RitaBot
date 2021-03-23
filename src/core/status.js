492afe78c60b76aefce150d56e5ab5bcd7a04c2a
hackerzz69 <80613732+hackerzz69@users.noreply.github.com>
2021-03-23 03:45:37 -0500

    Update status.js

--- src/core/status.js
+++ src/core/status.js
@@ -11,7 +11,7 @@ module.exports = function(bot, status, config, channel, writable = true)
          client.user.setActivity("!t help", {
   type: "STREAMING",
   url: "www.google.com"
-});
+})
             }
          });
       },
